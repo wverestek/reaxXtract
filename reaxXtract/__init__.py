286bc881808621b8a109a908d462bc67bd47c4dc
wverestek <wolfgang.verestek@web.de>
2026-03-11 00:59:56 +0100

    fix import of non class functions

--- reaxXtract/__init__.py
+++ reaxXtract/__init__.py
@@ -1,7 +1,14 @@
 # Package initializer for reaxXtract
 # Expose the main API class at package level.
 
-from .core import ReaxXtract
+from .utils import (DEFAULT_COLOR, ELEM2HEX, ON2ELEM, ON2HEX)
+from .core import (ReaxXtract, 
+                   renumber_and_count_rxns, filter_transient_reactions, remove_atoms_by_type, remove_atoms_by_pattern, 
+                   plot_rxns, get_degrees, find_minimum_cycle_basis)
 
-__all__ = ["ReaxXtract"]
+__all__ = ['DEFAULT_COLOR', 'ELEM2HEX', 'ON2ELEM', 'ON2HEX', 
+           'ReaxXtract',
+           'renumber_and_count_rxns', 'filter_transient_reactions', 'remove_atoms_by_type', 'remove_atoms_by_pattern', 
+           'plot_rxns', 'get_degrees', 'find_minimum_cycle_basis']
+
 __version__ = "0.0.1"
